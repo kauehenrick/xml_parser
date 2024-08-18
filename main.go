f7ace8396998ef6d3ebb6588f5873838b2e9afda
kauehenrick <kauehenrick@proton.me>
2024-08-18 14:37:31 -0300

    feat: finished v 1.0

--- main.go
+++ main.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 	"os"
-	"strings"
+	"path/filepath"
+	"regexp"
 )
 
 func main() {
-	readFiles(".")
-}
+	dir := "./"
+	novoCNPJ := "49345005000145"
 
-func readFiles(dir string)  {
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, file := range files {
-		endsWith := strings.HasSuffix(file.Name(), ".xml")
-
-		if endsWith {
-			fmt.Println(file.Name())
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
+
+		if filepath.Ext(path) == ".xml" {
+			xmlData, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			xmlStr := string(xmlData)
+
+			xmlStr = regexp.MustCompile(`(?s)<dest>\s*<CNPJ>[^<]*</CNPJ>`).ReplaceAllString(xmlStr, `<dest><CNPJ>`+novoCNPJ+`</CNPJ>`)
+
+			xmlStr = regexp.MustCompile(`(?s)<nfeProc([^>]*)>`).ReplaceAllString(xmlStr, `<nfeProc$1 xmlns="http://www.portalfiscal.inf.br/nfe">`)
+			xmlStr = regexp.MustCompile(`(?s)<NFe([^>]*)>`).ReplaceAllString(xmlStr, `<NFe$1 xmlns="http://www.portalfiscal.inf.br/nfe">`)
+
+			xmlStr = regexp.MustCompile(`(?s)<(/?\w+)([^>]*)\s+xmlns="http://www.portalfiscal.inf.br/nfe"([^>]*)>`).ReplaceAllString(xmlStr, `<$1$2$3>`)
+
+			err = ioutil.WriteFile(path, []byte(xmlStr), 0644)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Arquivo %s processado e substituído\n", path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Erro ao processar arquivos: %v\n", err)
 	}
 }
