3c3caaf04bb18129714a369b73cea7b49109b055
kauehenrick <kauehenrick@proton.me>
2024-08-18 12:45:45 -0300

    refactor: now reading dir files

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
 import (
-	
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
+	readFiles(".")
+}
+
+func readFiles(dir string)  {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		endsWith := strings.HasSuffix(file.Name(), ".xml")
+
+		if endsWith {
+			fmt.Println(file.Name())
+		}
+	}
 }
