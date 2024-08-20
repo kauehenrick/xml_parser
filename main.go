d05ef8679463c18b036a8ca977f86a5e5f9d3e2a
Kauê Henrick <kauehenrick@proton.me>
2024-08-19 21:45:14 -0300

    refactor: adjust entries values

--- main.go
+++ main.go
@@ -11,23 +11,23 @@ import (
 func main() {
 	dir := "./"
 	novoDest := `<dest>
-<CPF>12345678910</CPF>
-<xNome>PRIQUITO</xNome>
-<enderDest>
-<xLgr>Rua 31 de Maio</xLgr>
-<nro>180</nro>
-<xBairro>Sitio Grande</xBairro>
-<cMun>2928901</cMun>
-<xMun>Sao Desiderio</xMun>
-<UF>BA</UF>
-<CEP>47825000</CEP>
-<cPais>1058</cPais>
-<xPais>Brasil</xPais>
-<fone>77999930046</fone>
-</enderDest>
-<indIEDest>9</indIEDest>
-<email>kauek78942@gmail.com</email>
-</dest>`
+					<CPF></CPF>
+					<xNome></xNome>
+					<enderDest>
+						<xLgr></xLgr>
+						<nro></nro>
+						<xBairro></xBairro>
+						<cMun></cMun>
+						<xMun></xMun>
+						<UF></UF>
+						<CEP></CEP>
+						<cPais></cPais>
+						<xPais></xPais>
+						<fone></fone>
+					</enderDest>
+					<indIEDest></indIEDest>
+					<email></email>
+			</dest>`
 
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
